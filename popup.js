6883a24ea149cd31bbbf26cef4677079205b75af
Dachow <hellowd93@163.com>
2016-09-12 23:35:48 +0800

    push work

---
+++ popup.js
@@ -0,0 +1,6 @@
+var xx = document.querySelector("#wrap");
+var yy = Math.random();
+var yy1 = Math.round(255 * Math.random());
+var yy2 = Math.round(255 * Math.random());
+var yy3 = Math.round(255 * Math.random());
+xx.style.background = "rgba(" + yy1 + "," + yy2 + "," + yy3 + "," + yy + ")";
